036f9a61fa4cfcf6a9cf34edc5f2b0ca85e3f201
Anton Kalista <anton.kalista@yahoo.com>
2017-07-10 17:17:50 -0400

    Test Change from Pycharm

--- league_builder.py
+++ league_builder.py
@@ -2,6 +2,7 @@
 # Author: Anton Kalista
 
 '''
+Project 1 Treehouse TechDegree
 You have volunteered to be the Coordinator for your town’s youth soccer league.
 As part of your job you need to divide the 18 children who have signed up for the league into three even teams
 - Dragons, Sharks and Raptors. In years past, the teams have been unevenly matched, so this year you are doing your best
